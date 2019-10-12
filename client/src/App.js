c65abfd9c04e7dd4b8e706ebbed7f7cce750cc69
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-12 15:18:33 -0400

    added mounting and state, searchbtns & player map

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import SearchButtons from './components/SearchButtons';
+import Players from './components/Players'
+
 
 
 
 export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      player: [{
+        country: '',
+        id: '',
+        name: '',
+        searches: ''
+      }]
+    }
+  }
+
+  componentDidMount() {
+    // fetches player data for state
+    this.fetchPlayerData()
+  }
 
   fetchPlayerData = () => {
-    axios.get('http://localhost:5000/api/players').then(res => console.log(res.data)).catch(err => console.log("error:", err))
+    axios.get('http://localhost:5000/api/players').then(res => this.setState({
+      player: res.data
+    })).catch(err => console.log("error:", err))
   }
 
 
@@ -17,6 +38,7 @@ export default class App extends Component {
         <header className="App-header">
           <h1>Players!</h1>
           <SearchButtons />
+          {this.state.player.map(athlete => <Players player={athlete}/>)}
         </header>
       </div>
     )
