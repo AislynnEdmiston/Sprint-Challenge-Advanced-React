ba4e7538b9a922d86f4d9fc31363042edde96826
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-14 20:15:22 -0400

    forgot to commit this

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../App.css'
 
 export default function Players(props) {
     return (
         <div>
-            <h1>player</h1>
+            <h3>{props.player.name}</h3>
+            <h4>{props.player.country}</h4>
+            <h4>{props.player.searches}</h4>
         </div>
     )
 }
+
+
+// this.state = {
+//     player: [{
+//       country: '',
+//       id: '',
+//       name: '',
+//       searches: ''
+//     }]
